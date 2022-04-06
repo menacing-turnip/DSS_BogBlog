71b717d41b54967d41d8d983ed784789854a40f8
menacing-turnip <73412347+menacing-turnip@users.noreply.github.com>
2022-04-06 17:04:27 +0100

    Add files via upload

--- systemcode/api/create_database.sql
+++ systemcode/api/create_database.sql
@@ -2,29 +2,45 @@ CREATE DATABASE bogblogdb;
 
 CREATE TABLE usertable (
    uid        uuid    DEFAULT   gen_random_uuid() PRIMARY KEY,
-   username   VARCHAR (255) NOT NULL,
-   email      VARCHAR (255) NOT NULL,
+   username   VARCHAR (255) NOT NULL UNIQUE,
+   email      VARCHAR (255) NOT NULL UNIQUE,
    password   VARCHAR (255) NOT NULL,
    profilePic TEXT,
-   lastLogin  DATE,
-   UNIQUE (uid, username, email)
+   lastLogin  DATE
 );
 
 CREATE TABLE posts (
    pid          uuid    DEFAULT   gen_random_uuid() PRIMARY KEY,
-   title        VARCHAR (255)     NOT NULL,
-   description  TEXT              NOT NULL,
+   title        VARCHAR (255)     NOT NULL UNIQUE,
+   description  TEXT              NOT NULL UNIQUE,
    photo        TEXT,
    username     VARCHAR (255)     NOT NULL,
    categories   VARCHAR (255) [],
    published_on DATE              NOT NULL,
-   edited_on    DATE,
-   UNIQUE (pid, title, description) 
+   edited_on    DATE
 );
 
 CREATE TABLE category (
    cid         uuid    DEFAULT   gen_random_uuid() PRIMARY KEY,
    name        VARCHAR (255)     NOT NULL 
 );
+
+INSERT INTO category (name)
+VALUES ('POLITICS');
+
+INSERT INTO category (name)
+VALUES ('RELIGION');
+
+INSERT INTO category (name)
+VALUES ('HEALTH');
+
+INSERT INTO category (name)
+VALUES ('TECHNOLOGY');
+
+INSERT INTO category (name)
+VALUES ('MOVIES');
+
+INSERT INTO category (name)
+VALUES ('TV SHOWS');
 
 DROP DATABASE bogblogdb;
--- systemcode/api/index.js
+++ systemcode/api/index.js
@@ -6,6 +6,7 @@ const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -23,6 +24,8 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
 
 app.use(cors());
 app.use(express.json());
+app.use("/images/", express.static(path.join(__dirname, "/images/")));
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
